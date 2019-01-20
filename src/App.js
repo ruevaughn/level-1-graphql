ae6570c68b1524c813a7d6a64706b1d78a632316
Chase Jensen <soccerchase@gmail.com>
2019-01-20 16:39:13 -0700

    Test query using the client directly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
 import logo from './logo.svg';
 import './App.css';
 
 const client = new ApolloClient({
   uri: 'https://api-uswest.graphcms.com/v1/cjr4aysrs4stw01dn3d2f1pxy/master'
 })
+
+const testQuery = gql`
+  {
+    posts {
+      id
+      title
+      body
+    }
+  }
+`;
+
+client.query({
+  query: testQuery
+}).then(res => console.log(res));
 
 class App extends Component {
   render() {
