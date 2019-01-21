a059a019f19698e91595b5b067f0e123803b9048
Chase Jensen <soccerchase@gmail.com>
2019-01-20 22:41:51 -0700

    #10 Variables in Queries

--- src/Posts/Post.js
+++ src/Posts/Post.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
 export default class Post extends Component {
   render() {
+    const { match } = this.props;
     return (
-      <div>
-        <h1>hi</h1>
-      </div>
+      <Query 
+        query={POST_QUERY}
+        variables={{
+          id: match.params.id
+        }}
+      >
+        {({ data, loading }) => {
+          if (loading) return 'Loading...';
+          const { post } = data;
+          return <h1>{post.title}</h1>
+        }}
+      </Query>
     );
   }
 }
+
+const POST_QUERY = gql`
+  query post($id: ID!) {
+    post(where: { id: $id }) {
+      id
+      title
+      body
+    }
+  }
+`;
--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -6,18 +6,22 @@ import { Link } from 'react-router-dom';
 export default class Posts extends Component {
   render() {
     return (
+      <ul>
       <Query query={POSTS_QUERY}>
-        {({loading, data}) => {
-          if (loading) return "Loading...";
-          const { posts } = data;
-          return posts.map(post => (
-            <Link key={post.id} to={`/post/${post.id}`}>
-              <h1>{post.title}</h1>
-            </Link>
-            )
-          );
-        }}
+          {({loading, data}) => {
+            if (loading) return "Loading...";
+            const { posts } = data;
+            return posts.map(post => (
+              <li key={post.id}>
+                <Link to={`/post/${post.id}`}>
+                  {post.title}
+                </Link>
+              </li>
+              )
+            );
+          }}
       </Query>
+    </ul>
     );
   }
 }
